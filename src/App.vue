6ccb36f2e1a4d43f5518d4572940d3fc8415075a
Josh <jsbusiness305@gmail.com>
2025-02-28 17:12:55 -0500

    added home and project routes, added home and proejcts views

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h3>Welcome</h3>
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home View</h1>
+    <a href="/projects">Projects</a>
+  </div>
+</template>
