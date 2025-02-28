8b0d2901a36742a2ff1b67ad0a665d78ef3879fa
Josh <jsbusiness305@gmail.com>
2025-02-28 18:02:03 -0500

    added params to the single page view

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 <template>
   <div>
-    <h1>Single Project View</h1>
+    <h1>Single Project View {{ route.params?.id }}</h1>
   </div>
 </template>
