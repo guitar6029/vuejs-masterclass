eaa1399723c10b4f08897ca529c06e73054f5703
Josh <jsbusiness305@gmail.com>
2025-02-28 17:52:57 -0500

    added dynamic imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/projects',
       name: 'projects',
-      component: ProjectsView,
+      component: () => import('../views/ProjectsView.vue'),
     },
   ],
 })
